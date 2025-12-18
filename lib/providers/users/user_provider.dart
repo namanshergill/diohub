36953dcf76b3afd5a0e59ae569820e0aaceff85e
Naman <namanpreet@gmail.com>
2025-12-18 16:21:20 +0530

    Update user provider for GraphQL migration

--- lib/providers/users/user_provider.dart
+++ lib/providers/users/user_provider.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
 
-import 'package:diohub/models/users/user_info_model.dart';
+import 'package:diohub/graphql/queries/users/__generated__/user_info.data.gql.dart';
 import 'package:diohub/providers/base_provider.dart';
 import 'package:diohub/services/users/user_info_service.dart';
 
-class UserProvider extends BaseDataProvider<UserInfoModel> {
+class UserProvider extends BaseDataProvider<GuserInfoData_user> {
   UserProvider(this._userName);
   final String _userName;
 
   @override
-  Future<UserInfoModel> setInitData({final bool isInitialisation = false}) =>
-      UserInfoService.getUserInfo(_userName);
+  Future<GuserInfoData_user> setInitData(
+          {final bool isInitialisation = false}) =>
+      UserInfoService.getUserInfoGraphQL(_userName);
 }
